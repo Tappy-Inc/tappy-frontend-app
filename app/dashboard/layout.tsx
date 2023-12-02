cf8e3e5fe65575644b1e59ad7ccd40841d1ff1f7
Gelo <chrisgelosulit@gmail.com>
2023-12-02 19:28:02 +0800

    feat: Add dashboard layout, loading, and page components

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,7 @@
+import { PropsWithChildren } from "react";
+
+const layout: React.FC<PropsWithChildren> = ({ children }) => {
+  return <main>{children}</main>;
+};
+
+export default layout;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const dashboard = () => {
+  return (
+    <div>
+      Dashboard
+      <Link href="/">Go back</Link>
+    </div>
+  );
+};
+
+export default dashboard;
