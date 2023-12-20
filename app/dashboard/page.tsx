0236f42e54c0851bfc720419752aea4aa796fad1
Gelo <chrisgelosulit@gmail.com>
2023-12-20 14:55:00 +0800

    refactor: Remove unnecessary session handling and console.log in dashboard page component. Add email field to User interface

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,18 +1,8 @@
-import { api } from "@/utils/api";
 import Link from "next/link";
 
 const dashboard = async () => {
-  const session = await api.auth.session();
-
-  console.log(session);
-
   return (
     <div>
-      {session.result === 200 ? (
-        <div>session active</div>
-      ) : (
-        <div>session not active</div>
-      )}
       <Link href="/">Go back</Link>
     </div>
   );
