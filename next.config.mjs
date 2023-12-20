9c1db248cbc52630740627d178c60ec3bdbf6cd6
Gelo <chrisgelosulit@gmail.com>
2023-12-20 20:03:12 +0800

    feat(next.config): add redirects for root URL to /dashboard

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ await import("./lib/env.mjs");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: process.env.DOCKER_ENABLED === "1" ? "standalone" : undefined,
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
